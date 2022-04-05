1f504cff521c7e91d948255862b33c53fe521b25
Adriano-konig <konigadri87@hotmail.comgit config --global user.email konigadri87@hotmail.com>
2022-04-05 16:44:42 -0300

    Complementei mais uma linda exercicio1

--- AulaVetores/src/Exercicio1.java
+++ AulaVetores/src/Exercicio1.java
@@ -4,13 +4,14 @@ public class Exercicio1 {
     public static void main(String[] args) {
         int soma =0;
 
-        int vetor[] = new int[5];
+        int vetor[] = new int[6];
 
         vetor[0] = 5;
         vetor[1] = 10;
         vetor[2] = 20;
         vetor[3] = 3;
         vetor[4] = 1;
+        vetor[5] = 8;
 
 
         for(int i =0;i < vetor.length; i++){
